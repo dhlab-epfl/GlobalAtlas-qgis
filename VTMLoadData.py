2f6ccd03e3f2d0bf1d2a1e4f2c8218baf778d81c
olivierdalang <olivier.dalang@epfl.ch>
2015-03-23 18:57:24 +0100

    remove license info

--- VTMLoadData.py
+++ VTMLoadData.py
@@ -9,15 +9,6 @@
         copyright            : (C) 2014 by Olivier Dalang
         email                : olivier.dalang@gmail.com
  ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 """
 # Import the PyQt and QGIS libraries
 from PyQt4.QtCore import *
